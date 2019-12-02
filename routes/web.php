877e93d4c19e36bb720d827f8a84f35c9c1a65d9
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2019-12-02 09:52:01 +0700

    test deploy

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,23 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-//ROUTES PENGINAPAN
-Route::get('/penginapan/lihat', 'PenginapanController@index');
-Route::get('/penginapan/tambah', 'PenginapanController@showTambah');
-Route::post('/penginapan/store', 'PenginapanController@store');
-Route::get('/penginapan/hapus/{id}', 'PenginapanController@delete');
-Route::get('/penginapan/ubah/{id}', 'PenginapanController@showUbah');
-Route::put('/penginapan/update/{id}', 'PenginapanController@update');
+Route::group(['middleware' => ['auth']], function(){
+    //ROUTES PENGINAPAN
+    Route::get('/penginapan/lihat', 'PenginapanController@index');
+    Route::get('/penginapan/tambah', 'PenginapanController@showTambah');
+    Route::post('/penginapan/store', 'PenginapanController@store');
+    Route::get('/penginapan/hapus/{id}', 'PenginapanController@delete');
+    Route::get('/penginapan/ubah/{id}', 'PenginapanController@showUbah');
+    Route::put('/penginapan/update/{id}', 'PenginapanController@update');
 
-//ROUTES KAMAR
-Route::get('/kamar/lihat', 'KamarController@index');
-Route::get('/kamar/tambah', 'KamarController@showTambah');
-Route::post('/kamar/store', 'KamarController@store');
-Route::get('/kamar/hapus/{id}', 'KamarController@delete');
-Route::get('/kamar/ubah/{id}', 'KamarController@showUbah');
-Route::put('/kamar/update/{id}', 'KamarController@update');
+    //ROUTES KAMAR
+    Route::get('/kamar/lihat', 'KamarController@index');
+    Route::get('/kamar/tambah', 'KamarController@showTambah');
+    Route::post('/kamar/store', 'KamarController@store');
+    Route::get('/kamar/hapus/{id}', 'KamarController@delete');
+    Route::get('/kamar/ubah/{id}', 'KamarController@showUbah');
+    Route::put('/kamar/update/{id}', 'KamarController@update');
+});
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
